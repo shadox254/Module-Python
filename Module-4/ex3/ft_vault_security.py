a74d31308ee39bef4a83c149c8319f8d462c55eb
Remy Ruiz <rruiz@c1r3p2.42lehavre.fr>
2026-01-12 18:09:56 +0100

    Module-Python

---
+++ Module-4/ex3/ft_vault_security.py
@@ -0,0 +1,16 @@
+def vault_security(f_file, s_file):
+    print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===")
+    print()
+    print("Initiating secure vault access...")
+    with open(f_file, "r") as f:
+        print("Vault connection established with failsafe protocols")
+        print()
+        print("SECURE EXTRACTION:")
+        print(f.read().replace("[", "{[}").replace("]", "{]}"))
+    print()
+    with open()
+
+if __name__ == "__main__":
+    f_file = "classified_data.txt"
+    s_file = 
+    vault_security(f_file, s_file)
